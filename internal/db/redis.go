6cf92d24a59974890b57353279401d480ee5445e
channabasavaballolli <160584550+channabasavaballolli@users.noreply.github.com>
2026-05-13 16:57:31 +0530

    Update redis.go

--- internal/db/redis.go
+++ internal/db/redis.go
@@ -17,7 +17,9 @@ func ConnectRedis() { //connects redis to go app
 
 	_, err := RedisClient.Ping(Ctx).Result() //Testing redis by sending a message
 	if err != nil {
-		panic(err)
+		// panic(err)
+		fmt.Println("Redis unavailable:", err)
+return
 	}
 
 	fmt.Println("Redis connected")
